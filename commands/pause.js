9d725d20d0d21de182a11be6b56a11860949109d
Brian <86499417+briankhoi@users.noreply.github.com>
2023-08-18 23:15:29 -0700

    resume command

--- commands/pause.js
+++ commands/pause.js
@@ -4,16 +4,16 @@ const { useQueue } = require("discord-player");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pause")
-        .setDescription("Pauses the current song being played"),
+        .setDescription("Pauses the queue"),
     async execute(interaction) {
         // await interaction.deferReply();
         try {
             const queue = useQueue(interaction.guild.id);
             if (queue.node.isPaused()) {
-                return interaction.reply("Queue is already paused!");
+                return interaction.reply("The queue is already paused!");
             } else {
                 queue.node.setPaused(true);
-                return interaction.reply("Queue has been paused!");
+                return interaction.reply("The queue has been paused!");
             }
         } catch (e) {
             return interaction.reply(`Something went wrong: ${e}`);
--- commands/resume.js
+++ commands/resume.js
@@ -1,10 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const { useQueue } = require("discord-player");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('resume')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("Resumes the queue"),
+    async execute(interaction) {
+        // await interaction.deferReply();
+        try {
+            const queue = useQueue(interaction.guild.id);
+            if (!queue.node.isPaused()) {
+                return interaction.reply("The queue is already playing!");
+            } else {
+                queue.node.setPaused(false);
+                return interaction.reply("The queue has been resumed!");
+            }
+        } catch (e) {
+            return interaction.reply(`Something went wrong: ${e}`);
+        }
+    },
 };
