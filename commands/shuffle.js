d0c4b5215d013873c1d40e270ff4ce84b005641e
Brian <86499417+briankhoi@users.noreply.github.com>
2023-08-19 02:07:08 -0700

    shuffle command

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,10 +1,20 @@
+const { useQueue } = require('discord-player');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shuffle')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Shuffles the queue'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		try {
+			const queue = useQueue(interaction.guild.id);
+			if (!queue) {
+				return interaction.reply("The queue is empty! Please add some songs to use this command");
+			}
+			queue.tracks.shuffle();
+			return interaction.reply("Shuffling the queue!");
+		} catch (e) {
+			return interaction.reply(`Something went wrong: ${e}`);
+		}
 	},
 };
--- commands/skipto.js
+++ commands/skipto.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('skipto')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+    data: new SlashCommandBuilder()
+        .setName("skipto")
+        .setDescription("Skips to the position in queue"),
+    async execute(interaction) {
+        await interaction.reply("Pong!");
+    },
 };
