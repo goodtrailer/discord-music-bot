ed9a3bdfe71f94a3eb8062c34c64b4f939b3c58b
Brian <86499417+briankhoi@users.noreply.github.com>
2023-09-04 04:39:15 -0700

    changed tabs to spaces in linting config

--- commands/np.js
+++ commands/np.js
@@ -1,43 +1,43 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('np')
-		.setDescription('Displays information about the current playing song'),
-	async execute(interaction) {
-		const queue = useQueue(interaction.guild.id);
-		if (!queue) {
-			return interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setDescription(
-							'The queue is empty! Please add some songs to use this command',
-						)
-						.setColor('e8d5ac'),
-				],
-			});
-		}
-		try {
-			await interaction.deferReply();
-			const song = queue.currentTrack;
-			const bar = queue.node.createProgressBar({
-				length: 20,
-				queue: false,
-			});
-			const songInfo = new EmbedBuilder()
-				.setTitle(song.title)
-				.setURL(song.url)
-				.setDescription(bar)
-				.setThumbnail(song.thumbnail)
-				.setFooter({
-					text: `Requested by ${song.requestedBy.username}`,
-					iconURL: song.requestedBy.avatarURL(),
-				})
-				.setTimestamp()
-				.setColor('e8d5ac');
-			return interaction.followUp({ embeds: [songInfo] });
-		} catch (e) {
-			return interaction.followUp(`Something went wrong!\n ${e}`);
-		}
-	},
+    data: new SlashCommandBuilder()
+        .setName('np')
+        .setDescription('Displays information about the current playing song'),
+    async execute(interaction) {
+        const queue = useQueue(interaction.guild.id);
+        if (!queue) {
+            return interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            'The queue is empty! Please add some songs to use this command',
+                        )
+                        .setColor('e8d5ac'),
+                ],
+            });
+        }
+        try {
+            await interaction.deferReply();
+            const song = queue.currentTrack;
+            const bar = queue.node.createProgressBar({
+                length: 20,
+                queue: false,
+            });
+            const songInfo = new EmbedBuilder()
+                .setTitle(song.title)
+                .setURL(song.url)
+                .setDescription(bar)
+                .setThumbnail(song.thumbnail)
+                .setFooter({
+                    text: `Requested by ${song.requestedBy.username}`,
+                    iconURL: song.requestedBy.avatarURL(),
+                })
+                .setTimestamp()
+                .setColor('e8d5ac');
+            return interaction.followUp({ embeds: [songInfo] });
+        } catch (e) {
+            return interaction.followUp(`Something went wrong!\n ${e}`);
+        }
+    },
 };
--- commands/shuffle.js
+++ commands/shuffle.js
@@ -2,33 +2,33 @@ const { useQueue } = require('discord-player');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('shuffle')
-		.setDescription('Shuffles the queue'),
-	async execute(interaction) {
-		const queue = useQueue(interaction.guild.id);
-		if (!queue) {
-			return interaction.reply({
-				embeds: [
-					new EmbedBuilder()
-						.setDescription(
-							'The queue is empty! Please add some songs to use this command',
-						)
-						.setColor('e8d5ac'),
-				],
-			});
-		}
-		try {
-			queue.tracks.shuffle();
-			return interaction.followUp({
-				embeds: [
-					new EmbedBuilder()
-						.setDescription('Shuffling the queue!')
-						.setColor('e8d5ac'),
-				],
-			});
-		} catch (e) {
-			return interaction.reply(`Something went wrong: ${e}`);
-		}
-	},
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffles the queue'),
+    async execute(interaction) {
+        const queue = useQueue(interaction.guild.id);
+        if (!queue) {
+            return interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(
+                            'The queue is empty! Please add some songs to use this command',
+                        )
+                        .setColor('e8d5ac'),
+                ],
+            });
+        }
+        try {
+            queue.tracks.shuffle();
+            return interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription('Shuffling the queue!')
+                        .setColor('e8d5ac'),
+                ],
+            });
+        } catch (e) {
+            return interaction.reply(`Something went wrong: ${e}`);
+        }
+    },
 };
